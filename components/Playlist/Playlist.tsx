692df98068a07acd042a5c03c92dd37535f5dccb
Stuart Williams <stuart@tickx.co.uk>
2020-04-03 14:29:17 +0100

    Created PlaylistHead component

--- components/Playlist/Playlist.tsx
+++ components/Playlist/Playlist.tsx
@@ -2,11 +2,10 @@ import "./Playlist.scss";
 
 import React from "react";
 import { connect, ConnectedProps } from "react-redux";
-import pluralize from "pluralize";
 import { RootState } from "../../redux";
 import { getFocusedPlaylist } from "../../redux/playlists";
+import PlaylistHead from "../PlaylistHead";
 import PlaylistTracks from "../PlaylistTracks";
-import { Tag } from "@blueprintjs/core";
 
 const mapState = (state: RootState) => ({
   playlist: getFocusedPlaylist(state)
@@ -21,36 +20,15 @@ type Props = PropsFromRedux & {
 };
 
 const Playlist = (props: Props) => {
-  if (!props.playlist.id) {
+  const { id, tracks } = props.playlist;
+
+  if (!id) {
     return null;
   }
 
-  const { name, images, owner, followers, tracks } = props.playlist;
-
   return (
     <div className="Playlist">
-      {/* // TODO: extract <PlaylistHead /> */}
-      <div className="PlaylistHead">
-        <img className="PlaylistHead__image" src={images?.[0]?.url} />
-        <div className="PlaylistHead__meta">
-          <div className="PlaylistHead__title">
-            <h3 className="bp3-heading">{name}</h3>
-            <div className="PlaylistHead__tag">
-              {!!followers.total && (
-                <Tag>
-                  {followers.total} {pluralize("Followers", followers.total)}
-                </Tag>
-              )}
-            </div>
-          </div>
-          <div className="bp3-running-text">
-            <div className="bp3-text-muted">{owner.display_name}</div>
-            <div className="bp3-text-muted bp3-text-small">
-              {tracks.total} Songs
-            </div>
-          </div>
-        </div>
-      </div>
+      <PlaylistHead {...props.playlist} />
       <PlaylistTracks tracks={tracks?.items} />
     </div>
   );
---
+++ components/PlaylistHead/PlaylistHead.tsx
@@ -0,0 +1,57 @@
+import "./PlaylistHead.scss";
+
+import React from "react";
+import pluralize from "pluralize";
+import {
+  Tag,
+  Button,
+  Menu,
+  MenuItem,
+  Popover,
+  Position
+} from "@blueprintjs/core";
+
+type Props = SpotifyApi.PlaylistObjectFull & {};
+
+const PlaylistHead = (props: Props) => {
+  const { name, images, owner, followers, tracks } = props;
+
+  const menu = (
+    <Menu>
+      <MenuItem icon="edit" text="Rename" />
+      <MenuItem icon="random" text="Randomise Order" />
+      <MenuItem icon="heart-broken" text="Unfollow" />
+    </Menu>
+  );
+
+  return (
+    <div className="PlaylistHead">
+      <img className="PlaylistHead__image" src={images[0].url} alt={name} />
+      <div className="PlaylistHead__meta">
+        <div className="PlaylistHead__title">
+          <h3 className="bp3-heading">{name}</h3>
+          <div className="PlaylistHead__tag">
+            {!!followers.total && (
+              <Tag>
+                {followers.total} {pluralize("Followers", followers.total)}
+              </Tag>
+            )}
+          </div>
+          <div className="PlaylistHead__menu">
+            <Popover content={menu} position={Position.RIGHT_BOTTOM}>
+              <Button icon="more" />
+            </Popover>
+          </div>
+        </div>
+        <div className="bp3-running-text">
+          <div className="bp3-text-muted">{owner.display_name}</div>
+          <div className="bp3-text-muted bp3-text-small">
+            {tracks.total} Songs
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaylistHead;
