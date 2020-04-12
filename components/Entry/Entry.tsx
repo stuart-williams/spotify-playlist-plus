d0d8db264625b68e45f62cd6c3d82c4aaa42e728
Stuart Williams <stuart@tickx.co.uk>
2020-04-12 16:50:05 +0100

    created main sass

--- components/Entry/Entry.tsx
+++ components/Entry/Entry.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Head from "next/head";
 
 export default () => {
   return (
-    <div>
-      <h1>Playlist +</h1>
-      <a href={process.env.AUTHORIZE_URL}>Login</a>
-    </div>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/css/main.css" />
+      </Head>
+      <div className="Entry">
+        <h1>Playlist +</h1>
+        <a href={process.env.AUTHORIZE_URL}>Login</a>
+      </div>
+    </>
   );
 };
