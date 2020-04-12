7b3da60a406207bf15abad29aaede92ebef585b4
Stuart Williams <stuart@tickx.co.uk>
2020-04-12 17:19:36 +0100

    entry

--- components/Entry/Entry.tsx
+++ components/Entry/Entry.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import Head from "next/head";
 
-export default () => {
-  return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="/css/main.css" />
-      </Head>
-      <div className="Entry">
-        <h1>Playlist +</h1>
-        <h3>Advanced playlist management for Spotify</h3>
-        <a className="button" href={process.env.AUTHORIZE_URL}>
-          Connect with Spotify
-        </a>
-      </div>
-    </>
-  );
-};
+export default () => (
+  <>
+    <Head>
+      <link rel="stylesheet" href="/css/main.css" />
+    </Head>
+    <div className="Entry">
+      <h1>Playlist +</h1>
+      <h3>Advanced playlist management for Spotify</h3>
+      <a className="button" href={process.env.AUTHORIZE_URL}>
+        Connect with Spotify
+      </a>
+    </div>
+  </>
+);
