8b3a214196b94e2c9093ada8c9d1bb02527be5ee
Stuart Williams <stuart@tickx.co.uk>
2020-04-13 13:44:25 +0100

    filter out playlists the user doesn't own

--- components/PlaylistsMenu/PlaylistsMenu.tsx
+++ components/PlaylistsMenu/PlaylistsMenu.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import classNames from "classnames";
 import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "../../redux";
+import { selectUser } from "../../redux/user";
 import { selectListOfPlaylists } from "../../redux/playlists";
 import Link from "next/link";
 import { Classes } from "@blueprintjs/core";
 
 const mapState = (state: RootState) => ({
+  user: selectUser(state),
   playlists: selectListOfPlaylists(state),
 });
 
@@ -15,23 +17,28 @@ const connector = connect(mapState);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type Props = PropsFromRedux & {
+  user: SpotifyApi.CurrentUsersProfileResponse;
   playlists: SpotifyApi.PlaylistObjectSimplified[];
 };
 
 const PlaylistsMenu = (props: Props) => {
-  const renderMenuItem = (playlist: SpotifyApi.PlaylistObjectSimplified) => (
-    <li key={playlist.id}>
-      <Link href="/playlist/[id]" as={`/playlist/${playlist.id}`}>
-        <a className={Classes.MENU_ITEM}>
-          <span
-            className={classNames(Classes.TEXT_OVERFLOW_ELLIPSIS, Classes.FILL)}
-          >
-            {playlist.name}
-          </span>
-        </a>
-      </Link>
-    </li>
-  );
+  const renderMenuItem = (playlist: SpotifyApi.PlaylistObjectSimplified) => {
+    if (playlist.owner.id !== props.user.id) {
+      return null;
+    }
+
+    const classes = classNames(Classes.TEXT_OVERFLOW_ELLIPSIS, Classes.FILL);
+
+    return (
+      <li key={playlist.id}>
+        <Link href="/playlist/[id]" as={`/playlist/${playlist.id}`}>
+          <a className={Classes.MENU_ITEM}>
+            <span className={classes}>{playlist.name}</span>
+          </a>
+        </Link>
+      </li>
+    );
+  };
 
   return (
     <div className="Playlists">
