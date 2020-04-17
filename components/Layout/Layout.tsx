b7496fc836e108c62b54c013e30442c786e284b7
Stuart Williams <stuart@tickx.co.uk>
2020-04-17 11:14:41 +0100

    hide account icon when not signed in

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,32 +1,65 @@
 import React, { ReactNode } from "react";
 import { NextSeo } from "next-seo";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../../redux";
+import { selectUser } from "../../redux/user";
 import {
   Alignment,
   Navbar,
   NavbarGroup,
   NavbarHeading,
+  Button,
+  Menu,
+  MenuItem,
+  Popover,
 } from "@blueprintjs/core";
 import Navigation from "../Navigation";
 import SEO from "../../common/seo";
 
-interface Props {
+const mapState = (state: RootState) => ({
+  user: selectUser(state),
+});
+
+const connector = connect(mapState);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type Props = PropsFromRedux & {
+  user: SpotifyApi.CurrentUsersProfileResponse;
   title?: string;
   primaryPanel: ReactNode;
-}
+};
 
-export default ({ title, primaryPanel }: Props) => (
-  <>
-    <NextSeo title={title ? `${title} | ${SEO.title}` : undefined} />
-    <div className="Layout">
-      <Navbar>
-        <NavbarGroup align={Alignment.LEFT}>
-          <NavbarHeading>{SEO.title}</NavbarHeading>
-        </NavbarGroup>
-      </Navbar>
-      <div className="Layout__body">
-        <Navigation />
-        <div className="Layout__primary-panel">{primaryPanel}</div>
+const Layout = ({ user, title, primaryPanel }: Props) => {
+  const accountMenu = (
+    <Menu>
+      <MenuItem text="Logout" icon="log-out" href="/logout" />
+    </Menu>
+  );
+
+  const accountIcon = !!user.id && (
+    <Popover content={accountMenu} autoFocus={false}>
+      <Button icon="user" minimal={true} />
+    </Popover>
+  );
+
+  return (
+    <>
+      <NextSeo title={title ? `${title} | ${SEO.title}` : undefined} />
+      <div className="Layout">
+        <Navbar>
+          <NavbarGroup align={Alignment.LEFT}>
+            <NavbarHeading>{SEO.title}</NavbarHeading>
+          </NavbarGroup>
+          <NavbarGroup align={Alignment.RIGHT}>{accountIcon}</NavbarGroup>
+        </Navbar>
+        <div className="Layout__body">
+          <Navigation />
+          <div className="Layout__primary-panel">{primaryPanel}</div>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
+
+export default connector(Layout);
