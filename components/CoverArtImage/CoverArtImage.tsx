2fdddeed2f5ec5128cc96843dc1faca14f7cfe65
Stuart Williams <stuart@tickx.co.uk>
2020-04-17 15:54:45 +0100

    lazy load cover art

--- components/CoverArtImage/CoverArtImage.tsx
+++ components/CoverArtImage/CoverArtImage.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
+import VisibilitySensor from "react-visibility-sensor";
 import Img from "react-image";
 import Icon, { notesIcon } from "../Icon";
 
 export default ({ className, src, alt }: React.HTMLProps<HTMLImageElement>) => {
+  const [isVisible, setIsVisible] = useState(false);
   const icon = <Icon {...notesIcon} width={32} height={32} />;
 
+  const handleVisibilityChange = (visible: boolean) => {
+    if (!isVisible) {
+      setIsVisible(visible);
+    }
+  };
+
   return (
-    <div className={classNames("CoverArtImage", className)}>
-      <Img src={src} alt={alt} loader={icon} unloader={icon} />
-    </div>
+    <VisibilitySensor onChange={handleVisibilityChange}>
+      <div className={classNames("CoverArtImage", className)}>
+        {isVisible && <Img src={src} alt={alt} loader={icon} unloader={icon} />}
+      </div>
+    </VisibilitySensor>
   );
 };
